51578315520f2ec53462fc115c5620e901087aa5
vamikag <166573421+vamikag@users.noreply.github.com>
2026-03-10 22:22:37 -0700

    Update category.js

--- js/category.js
+++ js/category.js
@@ -19,12 +19,33 @@ function initCategoryTemplate() {
 
   if (mapBox) {
     mapBox.querySelector(".placeholder-content").innerHTML = `
-      <h3>Category Map Placeholder</h3>
-      <p>This section will later contain Vamika's category / brand visualization.</p>
+      <h3>Restaurant Category Overview</h3>
+      <p>This section will visualize restaurant categories using foot traffic data.</p>
+
+      <ul>
+        <li><strong>Breakfast</strong> locations</li>
+        <li><strong>Lunch</strong> locations</li>
+        <li><strong>Dinner</strong> locations</li>
+        <li><strong>Take Out & Delivery Only</strong> locations</li>
+      </ul>
+
+      <p>These categories will later be displayed on the map using filters.</p>
     `;
   }
 
   if (chartBox) {
-    chartBox.innerHTML = `<span>Category ranking placeholder</span>`;
+    chartBox.innerHTML = `
+      <h3>Category Ranking</h3>
+      <p>A bar chart will show the number of locations for each category.</p>
+
+      <ul>
+        <li>Breakfast</li>
+        <li>Lunch</li>
+        <li>Dinner</li>
+        <li>Take Out & Delivery Only</li>
+      </ul>
+    `;
   }
 }
+
+
